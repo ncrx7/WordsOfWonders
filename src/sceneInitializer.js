37617a85b3b5bf133dcd045f857d20563b3d08d5
ncrx7 <buysal31@gmail.com>
2024-10-28 19:31:35 +0300

    feat: Added async methods inside to scene initializer class

--- src/sceneInitializer.js
+++ src/sceneInitializer.js
@@ -1,4 +1,4 @@
-import { Container, Sprite } from "pixi.js";
+import { Container, Sprite, Graphics } from "pixi.js";
 import { GAME_HEIGHT, GAME_WIDTH } from ".";
 
 //DEV-NOTE(BATUHAN UYSAL): ---I MAKE THIS CLASS SINGLETON TO HAVE ONLY ONE INSTANCE FOR UI ANIMATIONS ON THE WHOLE GAME---
@@ -14,18 +14,43 @@ class sceneInitializer {
 
     async SetScene(game) {
         console.log("set scene from scene initiliazer")
-        await this.SetBackground(game);
-      }
-    
-    SetBackground(game) {
+        await this.SetBackground(game, 1);
+        await this.SetTopLetterContainer(game);
+    }
+
+    SetBackground(game, delayAfterLoadBackground) {
         return new Promise((resolve) => {
             let background = Sprite.from("background");
             background.width = GAME_WIDTH;
             background.height = GAME_HEIGHT;
             game.addChild(background);
+
+            setTimeout(() => {
+                resolve();
+            }, delayAfterLoadBackground * 1000);
+        });
+    }
+
+    //DEV-NOTE-TODO(BATUHAN UYSAL): ---HERE CAN BE PROCEDURAL BY CREATING A GRID SYSTEM AND INCLUDES LETTER AND CELL
+    SetTopLetterContainer(game) {
+        return new Promise(async (resolve) => {
+            await this.CreateLetterCell(game);
+            await this.CreateLetterCell(game);
+            await this.CreateLetterCell(game);
             resolve();
         });
-      }
+    }
+
+    CreateLetterCell(game) {
+        return new Promise((resolve) => {
+            let letterCellSprite = Sprite.from("rect");
+            game.addChild(letterCellSprite);
+
+            setTimeout(() => {
+                resolve();
+            }, 400);
+        });
+    }
 }
 
 const sceneInitializerInstance = new sceneInitializer();
